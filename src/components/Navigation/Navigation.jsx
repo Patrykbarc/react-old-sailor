f7e39d4985f6769a4732dba8a3c5ca8243419fd3
Patrykbarc <108985559+Patrykbarc@users.noreply.github.com>
2024-02-01 14:28:45 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,33 @@
 export function Navigation() {
-   return (
-      <div className="top-0 z-100 flex justify-between align-middle container mx-auto py-5 text-white text-xl">
-         <a href="">
-            <img src="./src/assets/logo.png" alt="" />
-         </a>
-         <ul className="flex gap-7">
-            <li className="px-3 cursor-pointer hover:text-brandHover transition"><a href="#about">About us</a></li>
-            <li className="px-3 cursor-pointer hover:text-brandHover transition"><a href="#">Store</a></li>
-            <li className="px-3 cursor-pointer hover:text-brandHover transition"><a href="#">Contact</a></li>
-         </ul>
-         <a href="tel:+100200300" className="flex gap-3 text-brand hover:text-brandHover transition">
-            <img className="h-fit my-auto" src="./src/assets/call-icon.png" alt="" />
-            <span>+48 100 200 300</span>
-         </a>
-      </div >
-   )
+  return (
+    <div className="fixed top-0 z-10 w-full py-5">
+      <div className="container mx-auto flex justify-between text-xl text-white">
+        <a href="">
+          <img src="./src/assets/logo.png" alt="" />
+        </a>
+        <ul className="flex gap-7">
+          <li className="cursor-pointer px-3 transition hover:text-brandHover">
+            <a href="#about">About us</a>
+          </li>
+          <li className="cursor-pointer px-3 transition hover:text-brandHover">
+            <a href="#">Store</a>
+          </li>
+          <li className="cursor-pointer px-3 transition hover:text-brandHover">
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <a
+          href="tel:+100200300"
+          className="flex gap-3 text-brand transition hover:text-brandHover"
+        >
+          <img
+            className="my-auto h-fit"
+            src="./src/assets/call-icon.png"
+            alt=""
+          />
+          <span>+48 100 200 300</span>
+        </a>
+      </div>
+    </div>
+  );
 }
