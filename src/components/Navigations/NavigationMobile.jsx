4c00452350f8e4855d276b51831752b35e71460e
Patryk Barć <patrykbarc.design@gmail.com>
2024-02-03 20:56:52 +0100

    Mobile nav handle

--- src/components/Navigations/NavigationMobile.jsx
+++ src/components/Navigations/NavigationMobile.jsx
@@ -1,4 +1,3 @@
-// import { NavigationMobileIcons } from "./NavigationMobileIcons";
 import { HamburgerIcon } from "../../icons/HamburgerIcon/HamburgerIcon";
 import { CloseIcon } from "../../icons/CloseIcon/CloseIcon";
 import { useState } from "react";
@@ -6,23 +5,40 @@ import { useState } from "react";
 export function NavigationMobile({ navLinks }) {
   const [isNavShown, setIsNavShown] = useState(false);
 
+  function handleSetIsNavShown() {
+    const body = document.body;
+
+    setIsNavShown(!isNavShown);
+    isNavShown
+      ? (body.style.overflowY = "auto")
+      : (body.style.overflowY = "hidden");
+  }
   return (
     <>
-      <div
-        className="absolute right-0 z-10 cursor-pointer p-5"
-        onClick={() => setIsNavShown(!isNavShown)}
-      >
-        {isNavShown ? <CloseIcon /> : <HamburgerIcon />}
+      <div onClick={handleSetIsNavShown}>
+        {!isNavShown && (
+          <div className="fixed right-0 top-0 z-30 cursor-pointer p-5 md:hidden">
+            <HamburgerIcon />
+          </div>
+        )}
       </div>
+
       {isNavShown && (
-        <nav className="relative">
-          <div className="container mx-auto flex h-screen items-center justify-end text-white">
+        <nav className="fixed z-20 w-screen bg-current md:hidden">
+          <div className="container relative mx-auto flex h-screen items-center justify-end text-white">
+            <div
+              className="fixed right-0 top-0 z-30 cursor-pointer p-5"
+              onClick={handleSetIsNavShown}
+            >
+              <CloseIcon />
+            </div>
+
             <ul className="flex flex-col gap-4 pe-12 text-right">
               {navLinks.map((link) => (
                 <li
                   key={link.id}
-                  className="hover:text-brand-hover cursor-pointer text-6xl capitalize transition"
-                  onClick={() => setIsNavShown(false)}
+                  className="hover:text-brand-hover cursor-pointer text-5xl capitalize transition"
+                  onClick={handleSetIsNavShown}
                 >
                   <a href={`#${link.id}`}>{link.name}</a>
                 </li>
